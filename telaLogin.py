c5f0d6c21bea6b1f89c9f52de543c4958e630f07
unknown <rafael.sqr@hotmail.com>
2022-01-09 09:26:31 -0300

    Primeiro

---
+++ telaLogin.py
@@ -0,0 +1,23 @@
+from PySimpleGUI import PySimpleGUI as sg
+
+#Layout
+sg.theme('Reddit')
+layout = [
+    [sg.Text('Usuário'),sg.Input(key='usuario',size=(20,1))],
+    [sg.Text('Senha'),sg.Input(key='senha',password_char="*",size=(20,1))],
+    [sg.Checkbox('Salvar Login?')],
+    [sg.Button('Entrar')]
+]
+
+#Janela
+janela = sg.Window('Tela Login', layout)
+#Ler os eventos
+while True:
+    eventos, valores = janela.read()
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    if eventos == 'Entrar':
+        if valores ['usuario'] == 'Rafael' and valores ['senha'] == '123456':
+            print ('Bem Vindo!  ')
+
+
